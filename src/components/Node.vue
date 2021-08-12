f66545d4f647c4c867ff8add185a13bbba24505d
Michael Jefferies <michaeljefferies08@gmail.com>
2021-08-12 22:11:26 +0100

    Initial commit

---
+++ src/components/Node.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="node"></div>
+</template>
+
+<script>
+export default {
+  name: 'Node',
+  props: {
+    rowIndex: Number,
+    colIndex: Number,
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .node{
+    border: 2px solid black;
+    width: 20px; height: 20px;
+  }
+</style>
---
+++ src/components/PathfinderGrid.vue
@@ -0,0 +1,65 @@
+<template>
+    <div id="grid">
+      <div
+        class="row"
+        :key="rowIndex" 
+        v-for="(row, rowIndex) in rowCount">
+        <div
+          class="col"
+          :key="colIndex"
+          v-for="(col, colIndex) in colCount">
+            <Node
+              :rowIndex="rowIndex"
+              :colIndex="colIndex" />
+          </div>
+      </div>
+
+    </div>
+</template>
+
+<script>
+import Node from "./Node.vue"
+export default {
+    name: "PathfinderGrid",
+    components: {
+      Node
+    },
+    data(){
+      return {
+        nodes: [],
+        rowCount: 25,
+        colCount: 25,
+      }
+    },
+    mounted(){
+      this.$nextTick(() => {
+        for (let row = 0; row < this.rowCount; row++) {
+          const currentRow = [];
+          for (let col = 0; col < this.colCount; col++) {
+            currentRow.push([]);
+          }
+          this.nodes.push(currentRow)
+        }
+      });
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+  #grid {
+    border: 5px solid green;
+    display: inline-block;
+  }
+
+  .row {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  } 
+
+  .col {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+</style>
